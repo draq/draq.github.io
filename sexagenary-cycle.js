031f88511405f531e66a9f261fc671127dfcac83
draq <draq@590f.net>
2021-02-15 23:25:56 +0100

    Fix problem with year calculation

--- sexagenary-cycle.js
+++ sexagenary-cycle.js
@@ -1,15 +1,15 @@
 const SEXAGENARY_DAY_START_MS = Date.UTC(2021, 0, 15, 16);  // 16 Jan 2021 00:00:00 GMT+0800 (Beijing Time) in milliseconds since Unix epoch.
-const SEXAGENARY_MONTH_START_MS = Date.UTC(2018, 12, 6, 16); // 2018年12月07 00:00:00 GMT+0800 (Beijing Time) in milliseconds since Unix epoch.
-const SEXAGENARY_YEAR_START_MS = Date.UTC(1984, 02, 03, 16); // 1984年02月04日 00:00:00 GMT+0800 (Beijing Time) in milliseconds since Unix epoch.
+const SEXAGENARY_MONTH_START_MS = Date.UTC(2018, 11, 6, 16); // 2018年12月07 00:00:00 GMT+0800 (Beijing Time) in milliseconds since Unix epoch.
+const SEXAGENARY_YEAR_START_MS = Date.UTC(1984, 01, 03, 16); // 1984年02月04日 00:00:00 GMT+0800 (Beijing Time) in milliseconds since Unix epoch.
 const DAY_IN_MS = 24 * 3600 * 1000;
-const YEAR_IN_MS = DAY_IN_MS * 365.25;
+const YEAR_IN_MS = DAY_IN_MS * 365.24219;
 
 function getSexagenaryCycle(number) { // Start with 0
     number = Math.floor(number);
     if (number < 0) {
         throw "The number must be equal or larger than 1!";
     }
-    number = number % 60;
+    number %= 60;
     let stem = HEAVENLY_STEMS[number % 10]; 
     stem = `${stem[0]}<rt>${stem[1]}</rt>`;
     let branch = EARTHLY_BRANCHES[number % 12];
@@ -20,7 +20,6 @@ function getSexagenaryCycle(number) { // Start with 0
 function getSexagenaryYear(date_ms) {
     let period = date_ms - SEXAGENARY_YEAR_START_MS;
     period = Math.floor(period / YEAR_IN_MS);
-    period %= 60;
     console.debug(`Sexagenary Year = ${period}`);
     return getSexagenaryCycle(period);
 }
@@ -44,14 +43,13 @@ function getSolarTerm(longitude) {
 function getSexagenaryDay(date_ms) {
     let period = date_ms - SEXAGENARY_DAY_START_MS;
     period = Math.floor(period / DAY_IN_MS);
-    period %= 60;
     console.debug(`Sexagenary Day = ${period}`);
     return getSexagenaryCycle(period); 
 }
 
 function getSexagenaryDate(date, longitude) {
-    date_ms = Number(date);
-    return `${getSexagenaryYear(date_ms)}年 ${getSexagenaryMonth(date_ms, longitude)}月 ${getSexagenaryDay(date_ms, longitude)}日`;
+    let date_ms = Number(date);
+    return `${getSexagenaryYear(date_ms)}年 ${getSexagenaryMonth(date_ms, longitude)}月 ${getSexagenaryDay(date_ms)}日`;
 
 }
 
