eb43c01be19f44c208aea50fb064b4e569023a2f
draq <draq@590f.net>
2023-11-28 09:15:26 +0100

    Add divination algorithm

--- calendar/yijing.js
+++ calendar/yijing.js
@@ -18,16 +18,16 @@ const BAGUA_WITH_CAPTIONS = [
   ['卦象', '卦名', '拼音', "自然象征", "性情", "家族关系", "动物", "身体部位", "器官", "方位", "五行"],
 
   ["☰", "乾", "qián", "天", "健", "父", "马", "头", "脑", "南", "金"],
-  ["☱", "兑", "duì",  "泽", "悦", "少女", "羊", "口", "肺", "东南", "金"],
+  ["☱", "兑", "duì", "泽", "悦", "少女", "羊", "口", "肺", "东南", "金"],
 
-  ["☲", "离", "lí",   "火", "丽", "中女", "雉", "目", "心", "东", "火"],
+  ["☲", "离", "lí", "火", "丽", "中女", "雉", "目", "心", "东", "火"],
   ["☳", "震", "zhèn", "雷", "动", "长男", "龙", "足", "肝", "东北", "木"],
 
-  ["☴", "巽", "xùn",  "风", "入", "长女", "鸡", "股", "胆", "西南", "木"],
-  ["☵", "坎", "kǎn",  "水", "陷", "中男", "豕", "耳", "肾", "西", "水"],
+  ["☴", "巽", "xùn", "风", "入", "长女", "鸡", "股", "胆", "西南", "木"],
+  ["☵", "坎", "kǎn", "水", "陷", "中男", "豕", "耳", "肾", "西", "水"],
 
-  ["☶", "艮", "gěn",  "山", "止", "少男", "狗", "手", "胃", "西北", "土"],
-  ["☷", "坤", "kūn",  "地", "顺", "母", "牛", "腹", "脾", "北", "土"],
+  ["☶", "艮", "gěn", "山", "止", "少男", "狗", "手", "胃", "西北", "土"],
+  ["☷", "坤", "kūn", "地", "顺", "母", "牛", "腹", "脾", "北", "土"],
 ];
 [
   "☰", "乾", "天", "健", "父", "马", "头", "脑", "南", "西北", "金", "111", "U+2630",
@@ -87,4 +87,115 @@ function getGua(index, withLink = true) {
     html = `<a href="https://ctext.org/book-of-changes/${X64[3][index]}/ens", target="_blank">${html}</a>`;
   }
   return html;
+}
+
+function yarrow(debug = false) {
+
+  function printys(ys, remark) {
+    console.debug(`[${ys[0]}, \t${ys[1]}, \t${ys[2]}, \t${ys[3]}, \t${ys[4]}, \t${ys[5]}, \t${ys[6]}] \t${remark}`);
+    return;
+  }
+
+  function getRandomInt(a, b) {
+    // The formula Math.floor(Math.random() * (max - min + 1)) + min ensures that the result is in the inclusive range [min, max]
+    return Math.floor(Math.random() * (b - a + 1) + a);
+  }
+
+  function ys_round(ys, round, debug = false) {
+    if (debug) {
+      console.debug(`=============== Round ${round} ===============`);
+      console.debug('[src, \tsky, \tleft, \thuman, \tearth, \tright, \tbin] \tremark');
+      // Generate a number somewhere in between 1/3 to 2/3 as humans do not trick
+      printys(ys, "Starting");
+    }
+
+    ys[1] = getRandomInt(ys[0] / 3, ys[0] * 2 / 3);
+    ys[4] = ys[0] - ys[1];
+    ys[0] = ys[0] - ys[1] - ys[4];
+    if (debug) printys(ys, "Separate into two");
+
+    ys[3] = 1;
+    ys[1] = ys[1] - ys[3];
+    if (debug) printys(ys, "and with one as human");
+
+    ys[2] = ys[1] % 4;
+    if (ys[2] === 0) {
+      ys[2] = 4;
+    }
+    ys[1] = ys[1] - ys[2];
+    if (debug) printys(ys, "then 4 by 4 and sky behind ...");
+
+    ys[5] = ys[4] % 4;
+    if (ys[5] === 0) {
+      ys[5] = 4;
+    }
+    ys[4] = ys[4] - ys[5];
+    if (debug) printys(ys, "then 4 by 4 and earth behind ...");
+
+    ys[6] += ys[2] + ys[3] + ys[5];
+    ys[2] = 0;
+    ys[3] = 0;
+    ys[5] = 0;
+    ys[0] = ys[1] + ys[4];
+    ys[1] = 0;
+    ys[4] = 0;
+    if (debug) printys(ys, "complete the cycle ...");
+
+    const checkSum = ys.reduce((partialSum, a) => partialSum + a, 0);
+    if (checkSum !== 49) {
+      console.error(`Checksum is ${checkSum} instead of 49.`)
+    }
+
+    return ys;
+  }
+
+  let ys = [0, 0, 0, 0, 0, 0, 0];
+  ys[0] = 55;
+  //console.debug(ys, "The number of heaven and earth is 55");
+  ys[0] -= 6;
+  //console.debug(ys, "only 49 is used");
+
+  // Round 1 needs to ensure mod 4 cannot return 0 and cannot have 0 
+  // wiki said cannot have 1 as well not sure about that
+
+  for (let i = 1; i < 4; i++) {
+    ys = ys_round(ys, i, debug);
+  }
+
+  let result = Math.floor(ys[0] / 4);
+
+  return result;
+
+}
+
+function divinate() {
+  // We build from bottom to top
+  let tossArray = [0, 0, 0, 0, 0, 0];
+
+  for (let line = 0; line < 6; line++) {
+    tossArray[line] = yarrow(debug = true);
+    console.debug(`Line ${line + 1}: toss is ${tossArray[line]}.`);
+  }
+
+  console.log(`Result is ${tossArray}.`)
+
+  function getLinesInReverse(tossArray) {
+    lines = '';
+    for (let line = 5; line >= 0; line--) {
+      let val = tossArray[line];
+      if (val === 6) {
+        lines += '<tr><td>6</td><td>==&nbsp;&nbsp;==</td><td>------</td></tr>';
+      } else if (val === 7) {
+        lines += '<tr><td>7</td><td>------</td><td>------</td></tr>';
+      } else if (val === 8) {
+        lines += '<tr><td>8</td><td>--&nbsp;&nbsp;--</td><td>--&nbsp;&nbsp;--</td></tr>';
+      } else if (val === 9) {
+        lines += '<tr><td>9</td><td>======</td><td>--&nbsp;&nbsp;--</td></tr>';
+      }
+    }
+    return lines;
+  }
+
+  // Assuming tossArray is already defined
+  return getLinesInReverse(tossArray);
 }
